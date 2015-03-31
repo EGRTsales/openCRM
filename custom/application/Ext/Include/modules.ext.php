2a21e7b364522fd099273f25a7d97eaab92e815f
unknown <bam1@erd18a.egr.local>
2015-03-31 15:15:18 +0200

    Clear Repos

--- custom/application/Ext/Include/modules.ext.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
-
-$moduleList['TRSubscribers'] = 'TRSubscribers';
-$beanList['TRSubscribers'] = 'TRSubscriber';
-$beanFiles['TRSubscriber'] = 'modules/TRSubscribers/TRSubscriber.php';
-?>
